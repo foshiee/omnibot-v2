ba3f44617ebf9acbef3cb819896f19949fbb14ca
foshiee-pydev <joshua.blair1@outlook.com>
2021-08-25 00:01:44 +0100

    First commit for omnibot-v2 Some basic functionality in place: xp & levels, welcome message, message sniping, setstatus, ping.

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -10,27 +10,30 @@ class Welcome(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
-        nt = int(time.time())
-        channel = self.bot.get_channel(865253224135393294)
-        val = (member.id, member.name, member.guild.id, 0, 0, 1, 1, 0, 0, 1, nt)
-        if not await check_table_exists("members"):
-            await query(returntype="commit", sql="""CREATE TABLE IF NOT EXISTS members (diwor INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(diwor), member_id bigint, 
-                                                member_name varchar(40), guild_id bigint, exp bigint, month_exp bigint, lvl int, 
-                                                month_lvl int, prestige int, coins int, can_mention int, rank_posttime bigint)""")
+        if not member.bot:
+            nt = int(time.time())
+            channel = self.bot.get_channel(865253224135393294)
+            val = (member.id, member.name, member.guild.id, 0, 0, 1, 1, 0, 0, 1, nt)
+            if not await check_table_exists("members"):
+                await query(returntype="commit", sql="""CREATE TABLE IF NOT EXISTS members (diwor INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(diwor), member_id bigint, 
+                                                    member_name varchar(40), guild_id bigint, exp bigint, month_exp bigint, lvl int, 
+                                                    month_lvl int, prestige int, coins int, can_mention int, rank_posttime bigint)""")
 
-            await query(returntype="commit", sql="""INSERT INTO members (member_id, member_name, guild_id, exp, 
-                                                month_exp, lvl, month_lvl, prestige, coins, can_mention) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""", params=val)
-        else:
-            result = await query(returntype="one", sql="""SELECT COUNT(*) FROM members WHERE member_id = %f""", params=member.id)
-            print(result)
-            if result[0] >= 1:
-                pass
+                await query(returntype="commit", sql="""INSERT INTO members (member_id, member_name, guild_id, exp, 
+                                                    month_exp, lvl, month_lvl, prestige, coins, can_mention, rank_posttime) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""", params=val)
             else:
-                await query(returntype="commit", sql="""INSERT INTO members (member_id, member_name, guild_id, exp,
-                                                    month_exp, lvl, month_lvl, prestige, coins, can_mention) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""", params=val)
+                result = await query(returntype="one", sql="SELECT COUNT(*) FROM members WHERE member_id = '" + str(member.id) + "'")
+                print(result)
+                if result[0] >= 1:
+                    pass
+                else:
+                    await query(returntype="commit", sql="""INSERT INTO members (member_id, member_name, guild_id, exp,
+                                                        month_exp, lvl, month_lvl, prestige, coins, can_mention, rank_posttime) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""", params=val)
 
-        if not channel:
-            return
+            if not channel:
+                return
+            else:
+                await channel.send(f":wave: | {member.display_name} has become an OG!")
 
 
 def setup(bot: commands.Bot):
