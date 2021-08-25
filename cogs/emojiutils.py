9fa6f63456ab615c40e238c88d955e97f16c56d4
foshiee-pydev <joshua.blair1@outlook.com>
2021-08-25 15:31:02 +0100

    Added cogs.emojiutils Contains a function for getting an emoji object using str(name) or int(id) Edit cogs.coins Adds omnicoin emoji to messages sent to bot. Normal coin emoji used if omnicoin emoji not found.

---
+++ cogs/emojiutils.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+from cogs.log import log
+import traceback
+import sys
+
+
+async def get_emoji(emote, ctx: commands.Bot):
+    try:
+        for emoji in ctx.emojis:
+            if emoji.name == str(emote) or emoji.id == int(emote):
+                e_id = int(emoji.id)
+                e_object = ctx.get_emoji(e_id)
+                return e_object
+
+            return None
+    except:
+        print("Unable to get emoji")
+        log(str(sys.exc_info()[0]))
+        log(str(sys.exc_info()[1]))
+        log(str(sys.exc_info()[2]))
+        traceback.print_exc()
+        return None
