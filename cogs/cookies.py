37d09f16150d284a9d8594f9b1a3f3e31c881e1f
foshiee-pydev <joshua.blair1@outlook.com>
2022-04-09 14:44:09 +0100

    Light changes to cooldown message timers to show hours, minutes or seconds. Added stats cog to extensions in Omnibot-v2.py

--- cogs/cookies.py
+++ cogs/cookies.py
@@ -1,8 +1,13 @@
 import asyncio
+import random
+
 import discord
 from discord.ext import commands
 from cogs.dbutils import query
 from cogs.emojiutils import get_emoji
+import traceback
+import sys
+from cogs.log import log
 
 
 class Cookie(commands.Cog):
@@ -11,65 +16,86 @@ class Cookie(commands.Cog):
 
     @commands.command(name="cookie")
     @commands.cooldown(rate=1, per=79200, type=commands.BucketType.member)
-    async def cookie(self, ctx, member: discord.Member = None, option=None):
-        cookiespin = await get_emoji(880509570978553856, self.bot)
-        if cookiespin is None:
-            cookiespin = ":cookie:"
+    async def cookie(self, ctx, member: discord.Member = None):
+        try:
+            cookiespin = await get_emoji(880509570978553856, self.bot)
+            if cookiespin is None:
+                cookiespin = ":cookie:"
 
-        cookiemonster = await get_emoji(596349000056307742, self.bot)
-        if cookiemonster is None:
-            cookiemonster = ":cookie:"
+            cookiemonster = await get_emoji(596349000056307742, self.bot)
+            if cookiemonster is None:
+                cookiemonster = ":cookie:"
 
-        if option is None:
+            cookie_types = ['chocolate', 'choc-chip', 'M&M encrusted', 'gingerbread', 'slightly broken',
+                            'half-eaten', 'pre-licked', 'golden', 'homemade', 'dark chocolate pistachio sea salt',
+                            'cinnamon roll sugar', 'brown butter oatmeal', 'kitsilano', 'brown butter bourbon spice',
+                            'peanut butter & jelly', 'caramel pecan', 'apricot pistachio oatmeal', 'pie crust',
+                            'nutella lava', 'almond & raspberry jam', 'million dollar', 'vanilla bean',
+                            'blueberry shortbread', 'red velvet', 'giant', 'tiny', 'small', 'double dark chocolate',
+                            'white chocolate macadamia', 'white chocolate coconut pecan']
+            selected_cookie = random.choice(cookie_types)
+
             if member is None:
-                await ctx.send(f"Use +cookie @username to give a user a cookie once per day, or keep it to yourself.")
+                await ctx.send(f"Use +cookie <username> to give a user a cookie once per day, or keep it to yourself.")
+                ctx.command.reset_cooldown(ctx)
             elif member.bot:
-                await ctx.send(f":robot:  |  Sorry, robots can't eat cookies made from organic material."
+                await ctx.send(f":robot:  Sorry, robots can't eat cookies made from organic material."
                                f" _sad beep boop_.")
+                ctx.command.reset_cooldown(ctx)
+            elif member is ctx.message.author:
+                val = (ctx.guild.id, member.id)
+                result = await query(returntype="one", sql="SELECT cookie_k FROM members WHERE"
+                                                           " guild_id = %s AND member_id = %s", params=val)
+
+                cookie_k = result[0]
+
+                await query(returntype="commit", sql="UPDATE members SET cookie_k = '" + str(cookie_k + 1) +
+                                                     "' WHERE guild_id = %s  AND member_id = %s", params=val)
+                await ctx.send(f"{cookiemonster}  {ctx.message.author.display_name} decided to keep their "
+                               f"{selected_cookie} cookie today. Om Nom Nom!")
             else:
                 val = (ctx.guild.id, member.id)
                 val2 = (ctx.guild.id, ctx.message.author.id)
-                result = await query(returntype="one", sql="SELECT cookie_s, cookie_k, cookie_r FROM members WHERE"
+                result = await query(returntype="one", sql="SELECT cookie_s, cookie_r FROM members WHERE"
                                                            " guild_id = %s AND member_id = %s", params=val)
+
                 if result is None:
-                    await ctx.send(f":question:  |  Hmm, I can't find a record for {member.display_name}. "
+                    await ctx.send(f":question:  Hmm, I can't find a record for {member}. "
                                    f"Have they spoken in this server before?")
+                    ctx.command.reset_cooldown(ctx)
+
                 else:
                     cookie_s = result[0]
-                    cookie_k = result[1]
-                    cookie_r = result[2]
+                    cookie_r = result[1]
+                    await query(returntype="commit", sql="UPDATE members SET cookie_r = '" + str(cookie_r + 1) +
+                                                         "' WHERE guild_id = %s  AND member_id = %s", params=val)
+                    await query(returntype="commit", sql="UPDATE members SET cookie_s = '" + str(cookie_s + 1) +
+                                                         "' WHERE guild_id = %s  AND member_id = %s", params=val2)
+                    await ctx.send(f"{cookiespin}  {ctx.message.author.display_name} has sent "
+                                   f"{member.display_name} a {selected_cookie} cookie!")
 
-                    if member.id == ctx.message.author.id:
-                        await query(returntype="commit", sql="UPDATE members SET cookie_k = '" + str(cookie_k+1) +
-                                                             "' WHERE guild_id = %s  AND member_id = %s", params=val)
-                        await ctx.send(f"{cookiemonster}  |  {ctx.message.author.display_name} decided to keep their "
-                                       f"cookie today. Om Nom Nom!")
-                    else:
-                        await query(returntype="commit", sql="UPDATE members SET cookie_r = '" + str(cookie_r+1) +
-                                                             "' WHERE guild_id = %s  AND member_id = %s", params=val)
-                        await query(returntype="commit", sql="UPDATE members SET cookie_s = '" + str(cookie_s + 1) +
-                                                             "' WHERE guild_id = %s  AND member_id = %s", params=val2)
-                        await ctx.send(f"{cookiespin}  |  {ctx.message.author.display_name} has sent "
-                                       f"{member.display_name} a cookie!")
-        elif option == "stats":
-            val = (ctx.guild.id, member.id)
-            result = await query(returntype="one", sql="SELECT cookie_s, cookie_k, cookie_r FROM members WHERE"
-                                                       " guild_id = %s AND member_id = %s", params=val)
-            cookie_s = result[0]
-            cookie_k = result[1]
-            cookie_r = result[2]
-
-            message = await ctx.send(f"Gathering ingredients and baking cookie statistics for {member.display_name}...")
-            await asyncio.sleep(2)
-            await message.edit(content=f"{cookiespin}  **{member.display_name}'s Cookie Statistics**\n\n"
-                                       f"Sent: {cookie_s}       Received: {cookie_r}        Kept: {cookie_k}")
+        except:
+            print("Something went wrong with cookies cog:", file=sys.stderr)
+            log(str(sys.exc_info()[0]))
+            log(str(sys.exc_info()[1]))
+            log(str(sys.exc_info()[2]))
+            traceback.print_exc()
 
     @cookie.error
     async def cookie_error(self, ctx: commands.Context, error):
         if isinstance(error, commands.CommandOnCooldown):
-            await ctx.message.delete(delay=5)
-            await ctx.send(f":hourglass:  |  You have already used your cookie for today! "
-                           f"The next batch will finish baking in {round(error.retry_after/60/60)} hours.")
+            if error.retry_after > 3600:
+                await ctx.send(f":hourglass:  You have already used your cookie for today, "
+                               f"{ctx.message.author.display_name}! "
+                               f"The next batch will finish baking in {round(error.retry_after / 60 / 60)} hours.")
+            elif 3600 > error.retry_after > 60:
+                await ctx.send(f":hourglass:  You have already used your cookie for today, "
+                               f"{ctx.message.author.display_name}! "
+                               f"The next batch will finish baking in {round(error.retry_after / 60)} minutes.")
+            else:
+                await ctx.send(f":hourglass:  You have already used your cookie for today, "
+                               f"{ctx.message.author.display_name}! "
+                               f"The next batch will finish baking in {round(error.retry_after)} seconds.")
 
 
 def setup(bot: commands.Bot):
