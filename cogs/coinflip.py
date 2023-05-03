337a1ef324b872e75d1d1b879079ea96f00954f5
foshiee <joshua.blair1@outlook.com>
2023-05-03 15:16:48 +0100

    going back to before coroutine changes

--- cogs/coinflip.py
+++ cogs/coinflip.py
@@ -17,14 +17,14 @@ class CoinFlip(commands.Cog, name="coinflip"):
 
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-        self.omnicoin = get_emoji("omnicoin", self.bot)
 
-    def create_embed(self, interaction: Interaction, title: Optional[str], description: Optional[str], 
+    async def create_embed(self, interaction: Interaction, title: Optional[str], description: Optional[str], 
                            colour: Colour, wallet, guess: Optional[Choice[str]]):
+        omnicoin = await get_emoji("omnicoin", self.bot)
         embed = Embed(title=title, description=description, colour=colour)
         embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar)
-        embed.set_thumbnail(url=self.omnicoin.url)
-        embed.add_field(name="Wallet", value=f"{wallet} {self.omnicoin}", inline=True)
+        embed.set_thumbnail(url=omnicoin.url)
+        embed.add_field(name="Wallet", value=f"{wallet} {omnicoin}", inline=True)
         if guess:
             embed.add_field(name="Your guess", value=guess.name)
         embed.set_footer(text=self.bot.user.display_name,icon_url=self.bot.user.display_avatar)
@@ -40,27 +40,28 @@ class CoinFlip(commands.Cog, name="coinflip"):
     ])
     @app_commands.command(name="coinflip")
     async def coin_flip(self, interaction: Interaction, guess: Choice[str], bet: int = None) -> None:
+        omnicoin = await get_emoji("omnicoin", self.bot)
         result = await query(returntype="one", sql="SELECT coins FROM members WHERE guild_id = %s AND member_id = %s", 
                              params=(interaction.guild_id, interaction.user.id))
         wallet = result[0]
         try:
             if bet is None or 0:
-                await interaction.response.send_message(f"You must bet at least 1 {self.omnicoin}")
+                await interaction.response.send_message(f"You must bet at least 1 {omnicoin}")
             elif bet > wallet:
-                await interaction.response.send_message(embed=self.create_embed(interaction, title="You can't afford that.",
+                await interaction.response.send_message(embed=await self.create_embed(interaction, title="You can't afford that.",
                                                                                 colour= Colour.brand_red(), wallet=wallet))
             else:
                 outcome = flip_coin()
                 if outcome is not guess.value:
                     wallet-=bet
-                    description = f"You lost {bet} {self.omnicoin}"
-                    await interaction.response.send_message(embed=self.create_embed(interaction, title=outcome.capitalize(),
+                    description = f"You lost {bet} {omnicoin}"
+                    await interaction.response.send_message(embed=await self.create_embed(interaction, title=outcome.capitalize(),
                                                                                     description=description, colour=Colour.brand_red(), 
                                                                                     wallet=wallet, guess=guess))
                 else:
                     wallet+=bet
-                    description = f"You won {bet*2} {self.omnicoin}"
-                    await interaction.response.send_message(embed=self.create_embed(interaction, title=outcome.capitalize(),
+                    description = f"You won {bet*2} {omnicoin}"
+                    await interaction.response.send_message(embed=await self.create_embed(interaction, title=outcome.capitalize(),
                                                                                     description=description, colour=Colour.brand_green(), 
                                                                                     wallet=wallet, guess=guess))      
                 await query(returntype="commit", sql="UPDATE members SET coins = %s WHERE guild_id = %s AND member_id = %s", 
