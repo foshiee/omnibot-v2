f826a97ce3d9f81d8e8d68eef078e666902b2a71
foshiee <joshua.blair1@outlook.com>
2023-05-04 12:59:28 +0100

    More embeds Moved send_cookie and send_rep inside the cog classes to make use of self.bot. Added embeds for send_cookie and greed functions in cookies.py

--- cogs/cookies.py
+++ cogs/cookies.py
@@ -1,7 +1,7 @@
 import random
 import discord
 from discord import app_commands, Interaction
-from discord.app_commands import AppCommandError, Cooldown, CommandOnCooldown, MissingRole
+from discord.app_commands import AppCommandError, MissingRole
 from discord.ext import commands
 from cogs.dbutils import query
 from cogs.emojiutils import get_emoji
@@ -18,69 +18,6 @@ cookie_types = ['chocolate', 'choc-chip', 'M&M encrusted', 'gingerbread', 'sligh
                 'white chocolate macadamia', 'white chocolate coconut pecan']
 
 
-async def send_cookie(self, interaction: Interaction, member: discord.Member) -> None:
-    cookiespin = await get_emoji("cookieSpin", self.bot)
-    if cookiespin is None:
-        cookiespin = ":cookie:"
-    try:
-        if member.bot:
-            await interaction.response.send_message(
-                f":robot:  Sorry, robots can't eat cookies made from organic material."
-                f" _sad beep boop_.", ephemeral=True, delete_after=20)
-        elif member.id is interaction.user.id:
-            await interaction.response.send_message(
-                f"{cookiespin}  Sorry, you can't send a cookie to yourself. Use the /cookie greed command "
-                f"instead!", ephemeral=True, delete_after=20)
-        else:
-            member_val = (interaction.guild_id, member.id)
-            user_val = (interaction.guild_id, interaction.user.id)
-            member_result = await query(returntype="one", sql="SELECT cookie_r FROM members WHERE"
-                                                              " guild_id = %s AND member_id = %s", params=member_val)
-            if member_result is None:
-                await interaction.response.send_message(f":question:  "
-                                                        f"Hmm, I can't find a record for {member.display_name}. "
-                                                        f"Have they spoken in this server before?", ephemeral=True)
-            else:
-                user_result = await query(returntype="one", sql="SELECT cookie_s, cookie_time FROM members WHERE "
-                                                                "guild_id = %s AND member_id = %s", params=user_val)
-
-                cookie_r = member_result[0]
-                cookie_s = user_result[0]
-                cookie_time = user_result[1]
-                new_time = interaction.created_at.replace(tzinfo=None)
-                delta = timedelta(hours=22)
-
-                if on_cooldown(cookie_time, new_time, delta):
-                    cd_sum = cookie_time + delta
-                    time_diff = cd_sum.timestamp() - new_time.timestamp()
-                    if time_diff > 3600:
-                        await interaction.response.send_message(
-                            f":hourglass:  You've run out of cookies for today, a fresh batch will finish baking in "
-                            f"{round(time_diff / 60 / 60)} hours.", ephemeral=True, delete_after=20)
-                    elif 3600 > time_diff > 60:
-                        await interaction.response.send_message(
-                            f":hourglass:  You've run out of cookies for today, a fresh batch will finish baking in "
-                            f"{round(time_diff / 60)} minutes.", ephemeral=True, delete_after=20)
-                    else:
-                        await interaction.response.send_message(
-                            f":hourglass:  You've run out of cookies for today, a fresh batch will finish baking in "
-                            f"{round(time_diff)} seconds.", ephemeral=True, delete_after=time_diff)
-                else:
-                    selected_cookie = random.choice(cookie_types)
-                    cookie_r += 1
-                    cookie_s += 1
-                    r_val = (cookie_r, interaction.guild_id, member.id)
-                    s_val = (cookie_s, new_time, interaction.guild_id, interaction.user.id)
-                    await query(returntype="commit", sql="UPDATE members SET cookie_r = %s WHERE guild_id = %s  "
-                                                         "AND member_id = %s", params=r_val)
-                    await query(returntype="commit", sql="UPDATE members SET cookie_s = %s, cookie_time = %s WHERE "
-                                                         "guild_id = %s  AND member_id = %s", params=s_val)
-                    await interaction.response.send_message(f"{cookiespin}  {interaction.user.display_name} has sent "
-                                                            f"{member.display_name} a {selected_cookie} cookie!")
-    except Exception as e:
-        print(e)
-
-
 class Cookies(commands.GroupCog, name="cookie"):
 
     def __init__(self, bot: commands.Bot) -> None:
@@ -88,9 +25,78 @@ class Cookies(commands.GroupCog, name="cookie"):
         self.ctx_menu = app_commands.ContextMenu(name="Send Cookie", callback=self.send_cookie_context)
         self.bot.tree.add_command(self.ctx_menu)
 
+    async def send_cookie(self, interaction: Interaction, member: discord.Member) -> None:
+        cookiespin = await get_emoji("cookieSpin", self.bot)
+        if cookiespin is None:
+            cookiespin = ":cookie:"
+        try:
+            if member.bot:
+                await interaction.response.send_message(
+                    f":robot:  Sorry, robots can't eat cookies made from organic material."
+                    f" _sad beep boop_.", ephemeral=True, delete_after=20)
+            elif member.id is interaction.user.id:
+                await interaction.response.send_message(
+                    f"{cookiespin}  Sorry, you can't send a cookie to yourself. Use the /cookie greed command "
+                    f"instead!", ephemeral=True, delete_after=20)
+            else:
+                member_val = (interaction.guild_id, member.id)
+                user_val = (interaction.guild_id, interaction.user.id)
+                member_result = await query(returntype="one", sql="SELECT cookie_r FROM members WHERE"
+                                                                " guild_id = %s AND member_id = %s", params=member_val)
+                if member_result is None:
+                    await interaction.response.send_message(f":question:  "
+                                                            f"Hmm, I can't find a record for {member.display_name}. "
+                                                            f"Have they spoken in this server before?", ephemeral=True)
+                else:
+                    user_result = await query(returntype="one", sql="SELECT cookie_s, cookie_time FROM members WHERE "
+                                                                    "guild_id = %s AND member_id = %s", params=user_val)
+
+                    cookie_r = member_result[0]
+                    cookie_s = user_result[0]
+                    cookie_time = user_result[1]
+                    new_time = interaction.created_at.replace(tzinfo=None)
+                    delta = timedelta(hours=22)
+
+                    if on_cooldown(cookie_time, new_time, delta):
+                        cd_sum = cookie_time + delta
+                        time_diff = cd_sum.timestamp() - new_time.timestamp()
+                        if time_diff > 3600:
+                            await interaction.response.send_message(
+                                f":hourglass:  You've run out of cookies for today, a fresh batch will finish baking in "
+                                f"{round(time_diff / 60 / 60)} hours.", ephemeral=True, delete_after=20)
+                        elif 3600 > time_diff > 60:
+                            await interaction.response.send_message(
+                                f":hourglass:  You've run out of cookies for today, a fresh batch will finish baking in "
+                                f"{round(time_diff / 60)} minutes.", ephemeral=True, delete_after=20)
+                        else:
+                            await interaction.response.send_message(
+                                f":hourglass:  You've run out of cookies for today, a fresh batch will finish baking in "
+                                f"{round(time_diff)} seconds.", ephemeral=True, delete_after=time_diff)
+                    else:
+                        selected_cookie = random.choice(cookie_types)
+                        cookie_r += 1
+                        cookie_s += 1
+                        r_val = (cookie_r, interaction.guild_id, member.id)
+                        s_val = (cookie_s, new_time, interaction.guild_id, interaction.user.id)
+
+                        sent_embed = discord.Embed(title="Cookie sent!", colour=discord.Colour.dark_gold())
+                        sent_embed.set_author(name=interaction.user.display_name,icon_url=interaction.user.display_avatar)
+                        sent_embed.set_thumbnail(url=cookiespin.url)
+                        sent_embed.add_field(name="Recipient", value=f"{member.display_name} {member.display_avatar}")
+                        sent_embed.add_field(name="Flavour", value=str(selected_cookie))
+                        sent_embed.set_footer(text=self.bot.user.display_name, icon_url=self.bot.user.display_avatar)
+
+                        await query(returntype="commit", sql="UPDATE members SET cookie_r = %s WHERE guild_id = %s  "
+                                                            "AND member_id = %s", params=r_val)
+                        await query(returntype="commit", sql="UPDATE members SET cookie_s = %s, cookie_time = %s WHERE "
+                                                            "guild_id = %s  AND member_id = %s", params=s_val)
+                        await interaction.response.send_message(embed=sent_embed)
+        except Exception as e:
+            print(e)
+
     @app_commands.command(name="send", description="Send a cookie to an OG member, once per day.")
     async def send(self, interaction: Interaction, member: discord.Member) -> None:
-        await send_cookie(self, interaction, member)
+        await self.send_cookie(self, interaction, member)
 
     @app_commands.command(name="greed", description="Keep your cookie to yourself! OMNOMNOM!!")
     async def greed(self, interaction: Interaction) -> None:
@@ -126,15 +132,21 @@ class Cookies(commands.GroupCog, name="cookie"):
             selected_cookie = random.choice(cookie_types)
             cookie_k += 1
             val = (cookie_k, new_time, interaction.guild_id, interaction.user.id)
+
+            greed_embed_desc = "You eat your cookie like a greedy cookie monster."
+            greed_embed = discord.Embed(title="OMNOMNOMNOMNOM!", description=greed_embed_desc, colour=discord.Colour.dark_blue())
+            greed_embed.set_author(name=interaction.user.display_name,icon_url=interaction.user.display_avatar)
+            greed_embed.set_thumbnail(url=cookiemonster.url)
+            greed_embed.add_field(name="Flavour", value=str(selected_cookie))
+            greed_embed.set_footer(text=self.bot.user.display_name, icon_url=self.bot.user.display_avatar)
+
             await query(returntype="commit", sql="UPDATE members SET cookie_k = %s, cookie_time = %s "
                                                  "WHERE guild_id = %s  AND member_id = %s", params=val)
-            await interaction.response.send_message(
-                f"{cookiemonster}  {interaction.user.display_name} is being a greedy cookie monster today, and eats "
-                f"their {selected_cookie} cookie themself! OMNOMNOMNOMNOM! ")
+            await interaction.response.send_message(embed=greed_embed)
 
     async def send_cookie_context(self, interaction: Interaction,
                                   member: Union[discord.Member, discord.User]) -> None:
-        await send_cookie(self, interaction, member)
+        await self.send_cookie(self, interaction, member)
 
     async def cog_app_command_error(self, interaction: Interaction, error: AppCommandError):
         if isinstance(error, MissingRole):
--- cogs/rep.py
+++ cogs/rep.py
@@ -7,69 +7,6 @@ from cogs.emojiutils import get_emoji
 from cogs.cooldown_utils import on_cooldown
 from datetime import timedelta
 
-async def send_rep(self, interaction: discord.Interaction, member: discord.Member):
-    clippy = await get_emoji("clippy", self.bot)
-    if clippy is None:
-        clippy = ":warning:"
-    epic = await get_emoji("epic", self.bot)
-    if epic is None:
-        epic = ":flower_playing_cards:"
-    try:
-        if member.bot:
-            await interaction.response.send_message(f":robot:  Sorry, you cannot big up a robot."
-                                                    f" _sad beep boop_.", ephemeral=True, delete_after=10)
-        elif interaction.user.id is member.id:
-            await interaction.response.send_message(f"{clippy}  Woah there! We know you're cool, "
-                                                    f"but you can't big up yourself.", ephemeral=True,
-                                                    delete_after=10)
-        else:
-            val = (interaction.guild.id, member.id)
-            result = await query(returntype="one", sql="SELECT rep, rep_time FROM members WHERE guild_id = %s AND "
-                                                       "member_id = %s", params=val)
-
-            if result is None:
-                await interaction.response.send_message(f":question:  "
-                                                        f"Hmm, I can't find a record for {member.display_name}. "
-                                                        f"Have they spoken in this server before?",
-                                                        ephemeral=True, delete_after=10)
-            else:
-                time_val = (interaction.guild_id, interaction.user.id)
-                time_result = await query(returntype="one", sql="SELECT rep_time FROM members WHERE guild_id = %s "
-                                                                "AND member_id = %s", params=time_val)
-                current_rep = result[0]
-                rep_time = time_result[0]
-                current_rep += 1
-                new_time = interaction.created_at.replace(tzinfo=None)
-                delta = timedelta(hours=22)
-
-                if on_cooldown(rep_time, new_time, delta):
-                    cd_sum = rep_time + delta
-                    time_diff = cd_sum.timestamp() - new_time.timestamp()
-                    if time_diff > 3600:
-                        await interaction.response.send_message(f":hourglass:  You have already given rep today! "
-                                                                f"Try again in {round(time_diff / 60 / 60)} hours.",
-                                                                ephemeral=True, delete_after=20)
-                    elif 3600 > time_diff > 60:
-                        await interaction.response.send_message(f":hourglass:  You have already given rep today! "
-                                                                f"Try again in {round(time_diff / 60)} minutes.",
-                                                                ephemeral=True, delete_after=20)
-                    else:
-                        await interaction.response.send_message(f":hourglass:  You have already given rep today! "
-                                                                f"Try again in {round(time_diff)} seconds.",
-                                                                ephemeral=True, delete_after=time_diff)
-                else:
-                    cr_val = (current_rep, interaction.guild_id, member.id)
-                    nt_val = (new_time, interaction.guild_id, interaction.user.id)
-                    await query(returntype="commit", sql="UPDATE members SET rep = %s "
-                                                         "WHERE guild_id = %s AND member_id = %s", params=cr_val)
-                    await query(returntype="commit", sql="UPDATE members SET rep_time = %s WHERE guild_id = %s AND "
-                                                         "member_id = %s", params=nt_val)
-                    await interaction.response.send_message(f"{epic}  {interaction.user.display_name} bigs up "
-                                                            f"{member.mention} and adds 1 to their rep counter. "
-                                                            f"{member.display_name} now has {current_rep} rep.")
-    except Exception as e:
-        print(e)
-
 
 class Rep(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
@@ -77,12 +14,76 @@ class Rep(commands.Cog):
         self.ctx_menu = ContextMenu(name='Rep', callback=self.rep_context, )
         self.bot.tree.add_command(self.ctx_menu)
 
+    async def send_rep(self, interaction: discord.Interaction, member: discord.Member):
+        clippy = await get_emoji("clippy", self.bot)
+        if clippy is None:
+            clippy = ":warning:"
+        epic = await get_emoji("epic", self.bot)
+        if epic is None:
+            epic = ":flower_playing_cards:"
+        try:
+            if member.bot:
+                await interaction.response.send_message(f":robot:  Sorry, you cannot big up a robot."
+                                                        f" _sad beep boop_.", ephemeral=True, delete_after=10)
+            elif interaction.user.id is member.id:
+                await interaction.response.send_message(f"{clippy}  Woah there! We know you're cool, "
+                                                        f"but you can't big up yourself.", ephemeral=True,
+                                                        delete_after=10)
+            else:
+                val = (interaction.guild.id, member.id)
+                result = await query(returntype="one", sql="SELECT rep, rep_time FROM members WHERE guild_id = %s AND "
+                                                        "member_id = %s", params=val)
+
+                if result is None:
+                    await interaction.response.send_message(f":question:  "
+                                                            f"Hmm, I can't find a record for {member.display_name}. "
+                                                            f"Have they spoken in this server before?",
+                                                            ephemeral=True, delete_after=10)
+                else:
+                    time_val = (interaction.guild_id, interaction.user.id)
+                    time_result = await query(returntype="one", sql="SELECT rep_time FROM members WHERE guild_id = %s "
+                                                                    "AND member_id = %s", params=time_val)
+                    current_rep = result[0]
+                    rep_time = time_result[0]
+                    current_rep += 1
+                    new_time = interaction.created_at.replace(tzinfo=None)
+                    delta = timedelta(hours=22)
+
+                    if on_cooldown(rep_time, new_time, delta):
+                        cd_sum = rep_time + delta
+                        time_diff = cd_sum.timestamp() - new_time.timestamp()
+                        if time_diff > 3600:
+                            await interaction.response.send_message(f":hourglass:  You have already given rep today! "
+                                                                    f"Try again in {round(time_diff / 60 / 60)} hours.",
+                                                                    ephemeral=True, delete_after=20)
+                        elif 3600 > time_diff > 60:
+                            await interaction.response.send_message(f":hourglass:  You have already given rep today! "
+                                                                    f"Try again in {round(time_diff / 60)} minutes.",
+                                                                    ephemeral=True, delete_after=20)
+                        else:
+                            await interaction.response.send_message(f":hourglass:  You have already given rep today! "
+                                                                    f"Try again in {round(time_diff)} seconds.",
+                                                                    ephemeral=True, delete_after=time_diff)
+                    else:
+                        cr_val = (current_rep, interaction.guild_id, member.id)
+                        nt_val = (new_time, interaction.guild_id, interaction.user.id)
+                        await query(returntype="commit", sql="UPDATE members SET rep = %s "
+                                                            "WHERE guild_id = %s AND member_id = %s", params=cr_val)
+                        await query(returntype="commit", sql="UPDATE members SET rep_time = %s WHERE guild_id = %s AND "
+                                                            "member_id = %s", params=nt_val)
+                        await interaction.response.send_message(f"{epic}  {interaction.user.display_name} bigs up "
+                                                                f"{member.mention} and adds 1 to their rep counter. "
+                                                                f"{member.display_name} now has {current_rep} rep.")
+        except Exception as e:
+            print(e)
+
+
     @app_commands.command(name="rep", description="Give recognition to a fellow OG. Send them rep and big them up.")
     async def rep(self, interaction: Interaction, member: discord.Member = None):
-        await send_rep(self, interaction, member)
+        await self.send_rep(self, interaction, member)
 
     async def rep_context(self, interaction: Interaction, member: discord.Member = None):
-        await send_rep(self, interaction, member)
+        await self.send_rep(self, interaction, member)
 
     async def cog_app_command_error(self, interaction: Interaction, error: AppCommandError):
         if isinstance(error, MissingRole):
